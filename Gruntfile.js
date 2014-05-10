5cec144ae4ae86014af500b59e5bebb86de41553
Sérgio Marcelino <sergiofilhow@gmail.com>
2014-05-10 10:46:33 -0300

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,58 @@
+/*jslint browser: true*/
+/*global module, alert*/
+
+module.exports = function (grunt) {
+
+    'use strict';
+    
+    var banner = '/* \n * JSUtil is licensed under MIT License @ 2014 \n * \n * Author: http://sergio.filho.org \n * More details on http://github.com/sergiofilhow/JSUtil \n * \n */ \n\n';
+    
+    // Project configuration.
+    grunt.initConfig({
+        pkg : grunt.file.readJSON('package.json'),
+        uglify : {
+            options : {
+                banner : banner
+            },
+            build : {
+                src : 'src/*.js',
+                dest : 'release/<%= pkg.name %>-<%= pkg.version %>.min.js'
+            }
+        },
+        
+        cssmin : {
+            options : {
+                banner : banner
+            },
+            css : {
+                src : 'release/<%= pkg.name %>-<%= pkg.version %>.css',
+                dest : 'release/<%= pkg.name %>-<%= pkg.version %>.min.css'
+            }
+        },
+        
+        concat : {
+            options : {
+                banner : banner
+            },
+            css : {
+                src : [
+                    'styles/*'
+                ],
+                dest : 'release/<%= pkg.name %>-<%= pkg.version %>.css'
+            },
+            js : {
+                src : [
+                    'src/*'
+                ],
+                dest : 'release/<%= pkg.name %>-<%= pkg.version %>.js'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    
+    grunt.registerTask('default', [ 'concat:css', 'cssmin:css', 'concat:js', 'uglify' ]);
+
+};
